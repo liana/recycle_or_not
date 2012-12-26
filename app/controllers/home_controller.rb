103f69ec30bd76695ff5918d5c9d072927d4699f
Liana Leahy <liana@practicallygreen.com>
2012-12-26 15:40:25 -0500

    init

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
+    authorize! :index, @user, :message => 'Not logged in.'
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,12 @@ module RecycleOrNot
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      
+
+      g.stylesheets false
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl
-      
-      
+
+
       g.view_specs false
       g.helper_specs false
     end
